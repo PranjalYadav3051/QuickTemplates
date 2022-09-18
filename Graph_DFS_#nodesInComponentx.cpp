743889cced3d407875575853feb166446a964ee5
PranjalYadav3051 <98053611+PranjalYadav3051@users.noreply.github.com>
2022-09-18 13:03:42 +0530

    commit

---
+++ Graph_DFS_#nodesInComponentx.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define M 1000000007
+
+#define pb push_back
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define all(x) (x).begin(), (x).end()
+#define ll             long long
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define w(x)            ll x; cin>>x; while(x--)
+#define google cout << "Case #" << test_num << ": "
+#define hi(x)           cout<<"hi"<<x<<endl;
+#define ACC(v)          accumulate(v.begin(),v.end(),0ll)
+
+#ifndef ONLINE_JUDGE
+#include "debug.hpp"
+#else
+#define pr(...){}
+#define debarr(a,n){}
+#define debmat(mat,row,col){}
+#endif
+
+void solve()
+{
+ 
+}
+
+
+int32_t main()
+{
+  fast;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif
+
+  w(t)
+  solve();
+  
+}
+ 
+
